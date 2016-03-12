2e7ac2f395be10265805a3e18e4435eb20e9b5d1
Dasaradhy <dasaradhy@gmail.com>
2016-03-12 20:40:49 +1100

    Setup some devise helpers and add a user login macro

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    firstname "MyString"
-    lastname "MyString"
-    nickname "MyString"
+    firstname {Faker::Name.first_name}
+    lastname {Faker::Name.last_name}
+    nickname {Faker::Name.first_name}
     provider "MyString"
     uid "MyString"
+    email { Faker::Internet.email }
+    password "test1234"
+    password_confirmation "test1234"
   end
 end
