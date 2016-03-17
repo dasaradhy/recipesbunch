c9669f712686bdded7ba5e7718906e85727f035a
Dasaradhy <dasaradhy@gmail.com>
2016-03-17 21:22:08 +1100

    adding recipe specs

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -16,4 +16,11 @@
 class Recipe < ActiveRecord::Base
   TIMES = ['5 minutes','10 minutes','15 minutes','20 minutes','30 minutes','45 minutes','1 hour','1:15 hours','1:30 hours','2:00 hours']
   belongs_to :chef
+  validates_presence_of :chef,:ingredients,:steps,:preparation_time
+  validate :preparation_time_is_valid
+
+  private
+  def preparation_time_is_valid
+    errors.add(:preparation_time,'should be a valid time') unless TIMES.include?(preparation_time)
+  end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -16,5 +16,30 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it 'has a valid factory' do
+    expect(create(:recipe)).to be_valid
+  end
+
+  it 'is invalid without any ingredients' do
+    expect(build(:recipe, ingredients:nil)).not_to  be_valid
+  end
+
+  it 'is invalid without any steps' do
+    expect(build(:recipe, steps:nil)).not_to  be_valid
+  end
+
+  it 'is invalid without a preparation_time' do
+    expect(build(:recipe,preparation_time: nil)).not_to  be_valid
+  end
+
+  it 'is invalid without a valid preparation_time' do
+    expect(build(:recipe,preparation_time: 'A few minutes')).not_to be_valid
+  end
+
+  it 'is invalid without a chef' do
+    expect(build(:recipe,chef: nil)).not_to be_valid
+  end
+
+
 end
