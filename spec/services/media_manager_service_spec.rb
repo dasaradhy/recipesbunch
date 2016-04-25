173b52aa80ebeebf6a93e2afdded05386d5a6d08
Dasaradhy Srinivasan <dasaradhy.srinivasan@fusionfactory.com>
2016-04-26 01:35:17 +1000

    adding tests for MediaManager

--- spec/services/media_manager_service_spec.rb
+++ spec/services/media_manager_service_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
-require 'carrierwave/test/matchers'
 
 describe 'MediaManager' do
-  
+
+  it "should load images from URL and should remove images successfully" do
+    uploaded_url = MediaManager.store_remote('https://robohash.org/recipesbunch.jpg?size=1000x1000')
+    expect(uploaded_url).to match(/png\z/)
+    expect(File.exist?("#{Rails.root}/public#{uploaded_url}")).to be true
+    MediaManager.remove_image(uploaded_url)
+    expect(File.exist?("#{Rails.root}/public#{uploaded_url}")).to be false
+  end
+
+  it "should load images from a file and should remove images successfully" do
+    image_file=File.open("#{Rails.root}/test/rb_test_image.jpg", 'rb' )
+    uploaded_url = MediaManager.store_file(image_file)
+    expect(uploaded_url).to match(/png\z/)
+    expect(File.exist?("#{Rails.root}/public#{uploaded_url}")).to be true
+    MediaManager.remove_image(uploaded_url)
+    expect(File.exist?("#{Rails.root}/public#{uploaded_url}")).to be false
+  end
+
 end
